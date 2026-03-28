1d7d89c3a2b173e9b1828b331a4beaea526cd61f
danigusmann <dgusmanguimaraes@gmail.com>
2026-03-28 15:38:05 -0300

    Add files via upload

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 def get_db_connection():
     conn = sqlite3.connect('database.db')
     conn.row_factory = sqlite3.Row 
-return conn
+    return conn
 
 def init_db():
     conn = get_db_connection()
